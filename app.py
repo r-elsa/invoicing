b1291088d280498b5aa636378ba106520d3da467
Elsa <elsaryynanen@outlook.com>
2023-01-27 20:25:21 +0200

    gitignore added

--- app.py
+++ app.py
@@ -6,3 +6,19 @@ app = Flask(__name__)
 def index():
     return "<p>Hello, World!</p>"
 
+
+@app.route("/page1")
+def page1():
+    return "Tämä on sivu 1"
+
+@app.route("/page2")
+def page2():
+    content = ""
+    for i in range(100):
+        content += str(i + 1) + " "
+    return content
+
+@app.route("/page/<int:id>")
+def page3(id):
+    return "Tämä on sivu " + str(id)
+
