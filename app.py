afe317d59c6a575f3609bd47b4d8b5864b49c6bc
Elsa <elsaryynanen@outlook.com>
2023-01-31 20:16:41 +0200

    connection to database

--- app.py
+++ app.py
@@ -2,19 +2,48 @@ from flask import Flask
 from flask import redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
-from os import getenv
+from dotenv import load_dotenv
+import os
+
+#from flask_migrate import Migrate
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv('DATABASE_URL')
-'postgresql+psycopg2://elsar'
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+
+#Model
+class Invoice(db.Model):
+    __tablename__="invoice"
+    id = db.Column(db.Integer, primary_key =True)
+    sender = db.Column(db.String(200), unique = True)
+    message = db.Column(db.String(200))
+    price_per_unit = db.Column(db.Integer)
+    amount_of_units = db.Column(db.Integer)
+    
+
+    def __init__(self, sender, message, price_per_unit, amount_of_units):
+        self.sender=sender
+        self.message=message
+        self.price_per_unit=price_per_unit
+        self.amount_of_units=amount_of_units
+
+
+
+
+
+
 @app.route("/")
 def index():
-    result = db.session.execute(text(f"SELECT content FROM invoices"))
+    db.create_all()
+    return 'it works'
+    """ result = db.session.execute(text(f"SELECT content FROM invoicess"))
     messages = result.fetchall()
-    return render_template("mall_index.html", count=len(messages), messages=messages) 
+    return render_template("mall_index.html", count=len(messages), messages=messages)   """
 
 @app.route("/new")
 def new():
@@ -27,6 +56,15 @@ def send():
     db.session.execute(sql, {"content":content})
     db.session.commit()
     return redirect("/")
+
+
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
 
 
 """ from flask import Flask, render_template, request
