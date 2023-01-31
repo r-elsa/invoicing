3b1c85f037f1440ea09072dab7b4716b83541a02
Elsa <elsaryynanen@outlook.com>
2023-01-31 15:38:12 +0200

    connection to database

--- app.py
+++ app.py
@@ -1,4 +1,35 @@
-from flask import Flask, render_template, request
+from flask import Flask
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+from os import getenv
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv('DATABASE_URL')
+'postgresql+psycopg2://elsar'
+
+db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
+    result = db.session.execute(text(f"SELECT content FROM invoices"))
+    messages = result.fetchall()
+    return render_template("mall_index.html", count=len(messages), messages=messages) 
+
+@app.route("/new")
+def new():
+    return render_template("mall_new.html")
+
+@app.route("/send", methods=["POST"])
+def send():
+    content = request.form["content"]
+    sql = "INSERT INTO invoices (content) VALUES (:content)"
+    db.session.execute(sql, {"content":content})
+    db.session.commit()
+    return redirect("/")
+
+
+""" from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -32,3 +63,4 @@ def page3(id):
 
 
 
+ """
