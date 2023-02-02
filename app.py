9d61697a48ddcc028c77e7565f74f90e9fe9cf3d
Elsa <elsaryynanen@outlook.com>
2023-02-02 22:36:13 +0200

    database tables

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 from dotenv import load_dotenv
 import os
-
 #from flask_migrate import Migrate
 
 load_dotenv()
@@ -16,7 +15,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-#Model
+#Models
 class Invoice(db.Model):
     __tablename__="invoice"
     id = db.Column(db.Integer, primary_key =True)
@@ -33,19 +32,87 @@ class Invoice(db.Model):
         self.amount_of_units=amount_of_units
 
 
+class Users(db.Model):
+    __tablename__="user"
+    id = db.Column(db.Integer, primary_key =True)
+    username = db.Column(db.String(200), unique = True)
+    email = db.Column(db.String(200))
+    password = db.Column(db.String(200))
+    
+
+    def __init__(self, username,email,password ):
+        self.username=username
+        self.email= email
+        self.password=password
 
 
+
+
+
+
+
+# Backend
 
 
 @app.route("/")
 def index():
     db.create_all()
-    return 'it works'
+    return render_template("login.html")
+
     """ result = db.session.execute(text(f"SELECT content FROM invoicess"))
     messages = result.fetchall()
     return render_template("mall_index.html", count=len(messages), messages=messages)   """
 
-@app.route("/new")
+
+
+@app.route("/dashboard", methods=["POST"])
+def dashboard():
+    email = request.form["email"]
+    username = request.form["username"]
+    password = request.form["password"]
+    #tietokantakysely -- users if username exists ---> RENDER all invoices of user
+    # if user not exists ----> create user 
+    #dashboard.html render alla invoices
+
+    return render_template("dashboard.html", username=username, password=password)
+
+
+
+
+@app.route("/new", methods=["POST"])
+def create_new_invoice():
+    return render_template("createinvoice.html")
+
+
+@app.route("/", methods=["POST"])
+def new_invoice():
+    #invoice_number = request.form["invoice"]
+    service = request.form["service"]
+
+    taxpercentage = request.form.getlist("taxpercentage")
+    message = request.form["message"]
+    # store values in database.
+
+    #sql = "INSERT INTO invoices (content) VALUES (:content)"
+    #db.session.execute(sql, {"content":content})
+    #db.session.commit()
+   
+    return render_template("invoice.html", service = service,
+                                            taxpercentage = taxpercentage,
+                                            message=message
+    )
+
+
+
+@app.route("/dashboard/invoice/<int:id>")
+def page3(id):
+    return "This is invoice number " + str(id)
+
+
+
+
+#################################################################
+@app.route("/mall_new")
 def new():
     return render_template("mall_new.html")
 
@@ -66,39 +133,3 @@ if __name__ == '__main__':
 
 
 
-
-""" from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    
-    return render_template("login.html")
-
-@app.route("/dashboard", methods=["POST"])
-def dashboard():
-    username = request.form["username"]
-    password = request.form["password"]
-    return render_template("dashboard.html", username=username, password=password)
-
-@app.route("/dashboard/create", methods=["POST"])
-def create():
-    invoice_number = request.form["invoice"]
-    taxvalue = request.form.getlist("taxvalue")
-    message = request.form["message"]
-    return render_template("invoice.html", invoice_number = invoice_number,
-                                            taxvalue=taxvalue,
-                                            message=message
-    )
-
-
-
-@app.route("/dashboard/invoice/<int:id>")
-def page3(id):
-    return "This is invoice number " + str(id)
-
-
-
-
- """
