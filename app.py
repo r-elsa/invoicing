8d42d621aaacee2ec204512efa953bab354ccd1c
Elsa <elsaryynanen@outlook.com>
2023-02-05 16:32:53 +0200

    query for user id after login and signup added

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 import os
 from datetime import datetime, timezone
 import re
-from models import User, Client, Project, Invoice,InvoiceItem, TaxType, Payment
 
 load_dotenv()
 
@@ -18,20 +17,63 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
+   
+    return redirect('/login')
+
+@app.route("/login")
+def login():
     db.create_all()
     return render_template("login.html")
+
 
 @app.route("/signup",  methods=["POST", "GET"])
 def signup():
     return render_template("signup.html")
 
+
 @app.route("/dashboard", methods=["POST"])
 def dashboard():      
     referralroute = request.referrer
-   
+    logged_user = 1000000
+  
+    
+    # sign up
+    if referralroute[-6:]=="signup":
+        username = request.form["username"]
+        password = request.form["password"]
+        #is username alredy taken
+        sql = "SELECT id, username FROM users WHERE username LIKE :username"
+        result = db.session.execute(text(sql), {"username":"%"+username+"%"})
+        soughtuser = result.fetchall()
+        if (len(soughtuser)) > 0:
+            render_template("signup.html", errormessage = "Username already taken")
+        else:     
+            email = request.form["email"]
+            sql = "INSERT INTO users (username, email, password) VALUES (:username, :email, :password)"
+            db.session.execute(text(sql), {"username":username, "email":email,"password":password})
+            db.session.commit()
+
+            sql = "SELECT id FROM users WHERE username LIKE :username"
+            result = db.session.execute(text(sql), {"username":"%"+username+"%"})
+            logged_user= result.fetchall()[0][0]
+         
+    
+    #login
+    elif referralroute[-5:]=="login":
+        username = request.form["username"]
+        password = request.form["password"]
+        sql = "SELECT id, email FROM users WHERE username LIKE :username AND password LIKE :password"
+        result = db.session.execute(text(sql), {"username":"%"+username+"%", "password":"%"+password+"%"})
+        soughtuser = result.fetchall()
+        if (len(soughtuser)) == 0:
+            render_template("login.html", errormessage = "Wrong username or password")
+        else:
+            logged_user = soughtuser[0][0]
+
+    
     # create invoice
-    if referralroute[-13:]=="createinvoice":
-        logged_user = 2 # user id - from session data
+    elif referralroute[-13:]=="createinvoice":
+        #logged_user = 2 # user id - from session data
         project_name = request.form["project_name"]  ## key to Project -name, not id
         client_name = request.form["client_name"] # connect to client name , not id
         summary = request.form["summary"]
@@ -46,33 +88,8 @@ def dashboard():
         db.session.execute(text(sql), {"logged_user":logged_user,  "project_name": project_name, "client_name": client_name, "summary":summary, "raised_date":raised_date, "due_date":due_date, "status":status, "tax_type": tax_type,"discount":discount,"comment":comment})
         db.session.commit()
      
-    # sign up
-    elif referralroute[-6:]=="signup":
-        username = request.form["username"]
-        password = request.form["password"]
-        #is username alredy taken
-        sql = "SELECT id, username FROM users WHERE username LIKE :username"
-        result = db.session.execute(text(sql), {"username":"%"+username+"%"})
-        soughtuser = result.fetchall()
-        if (len(soughtuser)) > 0:
-            returntemplate = render_template("signup.html", errormessage = "Username already taken")
-        else:     
-            email = request.form["email"]
-            sql = "INSERT INTO users (username, email, password) VALUES (:username, :email, :password)"
-            db.session.execute(text(sql), {"username":username, "email":email,"password":password})
-            db.session.commit()
-    
-    #login
-    else:
-        username = request.form["username"]
-        password = request.form["password"]
-        sql = "SELECT id, email FROM users WHERE username LIKE :username AND password LIKE :password"
-        result = db.session.execute(text(sql), {"username":"%"+username+"%", "password":"%"+password+"%"})
-        soughtuser = result.fetchall()
-        if (len(soughtuser)) == 0:
-            render_template("login.html", errormessage = "Wrong username or password")
 
-    logged_user = 2 
+
     sql = "SELECT id, project_name, client_name, due_date, status FROM invoices WHERE logged_user =:logged_user"
     result = db.session.execute(text(sql), {"logged_user":logged_user})
     all_invoices = result.fetchall()      
