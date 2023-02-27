b847b55530cf03ccbe49d28fc3a172b90cf5c3b1
Elsa <elsaryynanen@outlook.com>
2023-02-27 18:48:09 +0200

    SQLquery lines split to multiple lines

--- invoices.py
+++ invoices.py
@@ -6,8 +6,15 @@ from sqlalchemy import text
 
 
 def create_invoice(logged_user,project_name,client_name,summary, raised_date, due_date, status, tax_type, discount, comment, product_amount):
-    sql = "INSERT INTO invoices (logged_user, project_name, client_name, summary, raised_date, due_date, status, tax_type,discount,comment,product_amount) VALUES (:logged_user, :project_name, :client_name, :summary, :raised_date, :due_date, :status, :tax_type, :discount,:comment, :product_amount)"
-    db.session.execute(text(sql), {"logged_user":logged_user,  "project_name": project_name, "client_name": client_name, "summary":summary, "raised_date":raised_date, "due_date":due_date, "status":status, "tax_type": tax_type,"discount":discount,"comment":comment, "product_amount": product_amount})
+    sql = "INSERT INTO invoices (logged_user, project_name, client_name, summary," \
+          "raised_date, due_date, status, tax_type,discount,comment,product_amount) VALUES"\
+          "(:logged_user, :project_name, :client_name, :summary, :raised_date, :due_date, :status," \
+          " :tax_type, :discount,:comment, :product_amount)"
+    
+    db.session.execute(text(sql), {"logged_user":logged_user,  "project_name": project_name, "client_name": client_name, 
+    "summary":summary, "raised_date":raised_date, "due_date":due_date, "status":status, "tax_type": tax_type,"discount":discount,
+    "comment":comment, "product_amount": product_amount})
+
     db.session.commit()
    
 
--- products.py
+++ products.py
@@ -6,8 +6,10 @@ from sqlalchemy import text
 
 def add_product(invoice,user_id,name,description,price_per_unit,amount):
    
-    sql = "INSERT INTO products (invoice, user_id, name, description, price_per_unit, amount ) VALUES (:invoice, :user_id, :name, :description, :price_per_unit, :amount )"
-    db.session.execute(text(sql), {"invoice": invoice,"user_id": user_id,"name": name, "description": description, "price_per_unit": price_per_unit,"amount": amount })
+    sql = "INSERT INTO products (invoice, user_id, name, description, price_per_unit, amount ) VALUES (:invoice, :user_id,"\
+         ":name, :description, :price_per_unit, :amount )"
+    db.session.execute(text(sql), {"invoice": invoice,"user_id": user_id,"name": name, "description": description, 
+    "price_per_unit": price_per_unit,"amount": amount })
     db.session.commit()
 
 def return_all(user_id):
